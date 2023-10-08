636e4ad357c736081487f8ce243118baa0e0b88e
Craig Hart <i@craigary.net>
2023-10-09 00:10:24 +0800

    Fix style error on mobile

--- components/ui/sheet.jsx
+++ components/ui/sheet.jsx
@@ -5,6 +5,7 @@ import { cva } from 'class-variance-authority'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
+import { useEffect, useState } from 'react'
 
 const Sheet = SheetPrimitive.Root
 
@@ -49,22 +50,30 @@ const sheetVariants = cva(
 )
 
 const SheetContent = React.forwardRef(
-  ({ side = 'right', className, children, ...props }, ref) => (
-    <SheetPortal>
-      <SheetOverlay />
-      <SheetPrimitive.Content
-        ref={ref}
-        className={cn(sheetVariants({ side }), className)}
-        {...props}
-      >
-        {children}
-        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800">
-          <Cross2Icon className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </SheetPrimitive.Close>
-      </SheetPrimitive.Content>
-    </SheetPortal>
-  )
+  ({ side = 'right', className, children, ...props }, ref) => {
+    const [rootDom, setRootDom] = useState()
+    useEffect(() => {
+      const dom = document.querySelector('[data-is-root-theme="true"]')
+      setRootDom(dom)
+    }, [])
+
+    return (
+      <SheetPortal container={rootDom}>
+        <SheetOverlay />
+        <SheetPrimitive.Content
+          ref={ref}
+          className={cn(sheetVariants({ side }), className)}
+          {...props}
+        >
+          {children}
+          <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800">
+            <Cross2Icon className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </SheetPrimitive.Close>
+        </SheetPrimitive.Content>
+      </SheetPortal>
+    )
+  }
 )
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
