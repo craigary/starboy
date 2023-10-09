e74390ae112c6f9817b20d5cd79d964f4c2ad962
Craig Hart <i@craigary.net>
2023-10-10 04:54:22 +0800

    Minor tweaks

--- components/bento/social/SocialCard.jsx
+++ components/bento/social/SocialCard.jsx
@@ -4,12 +4,12 @@ import getLatestSocialPost from '@/lib/social/get-latest-social-post'
 const SocialCard = async () => {
   const data = await getLatestSocialPost()
 
-  const post = data.items[0]
+  // const post = data.items[0]
 
   return (
     <Card className="h-full p-3">
       <div className="flex h-full w-full items-center rounded-lg border p-4 text-3xl text-primary/30">
-        {post.title}
+        {/* {post.title} */}
       </div>
     </Card>
   )
