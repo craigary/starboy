1582d007898e919ff63ac4353af8a5b5bb5ab853
Craig Hart <i@craigary.net>
2023-10-18 11:48:22 +0800

    Minor changes...

--- components/bento/social/SocialAlt.jsx
+++ components/bento/social/SocialAlt.jsx
@@ -6,11 +6,11 @@ const SocialAltCard = async ({ delay = 0 }) => {
 
   return (
     <Card className="h-full p-3" delay={delay}>
-      <div className="relative h-full w-full  rounded-md border bg-background p-2 shadow-none transition-shadow hover:shadow-xl hover:shadow-muted/60 dark:bg-muted/20">
+      <div className="relative h-full w-full rounded-md border bg-transparent p-2 shadow-none transition-shadow hover:shadow-xl hover:shadow-muted/60 dark:bg-muted/20">
         <div className="mx-3 flex h-full items-center justify-center">
           <div className="flex items-end justify-center gap-3">
             <div className="h-8 w-8 shrink-0 rounded-full bg-cyan-200"></div>
-            <div className="rounded-xl rounded-bl-none border bg-background px-3 py-2 text-sm xl:text-base">
+            <div className="rounded-xl rounded-bl-none border bg-background px-3 py-2 text-sm">
               {post.title}
             </div>
           </div>
