2ccd98ccef181c5d1ee273dac6b9faf56e7cc9f0
Craig Hart <i@craigary.net>
2023-10-21 12:19:22 +0800

    change Link to a tag for external links

--- components/navigation/NavSection.jsx
+++ components/navigation/NavSection.jsx
@@ -24,25 +24,47 @@ const NavSection = ({ name, showTitle, navItems, setOpen }) => {
           const isActive = isActivePath(pathname, navItem.link)
           return (
             <li key={index}>
-              <Link
-                data-disabled={navItem.disabled}
-                className={cn(
-                  '[data-disabled]:cursor-not-allowed flex cursor-default items-center justify-between rounded-md px-2 py-2 text-sm transition-all hover:bg-[--accent-a3] active:scale-[98%] disabled:opacity-50',
-                  isActive && 'bg-[--accent-a3]'
-                )}
-                onClick={handleNavItemClick}
-                href={navItem.link}
-                target={navItem.external ? '_blank' : '_self'}
-              >
-                <div className="flex items-center justify-start">
-                  <Icon size="18" className="mr-2" stroke="1.5" />
-                  {navItem.name}
-                  {navItem.disabled && <Badge className="ml-2">soon</Badge>}
-                </div>
-                <div className="shrink-0">
-                  {navItem.external && <IconArrowUpRight size="16" />}
-                </div>
-              </Link>
+              {navItem.external ? (
+                <a
+                  data-disabled={navItem.disabled}
+                  className={cn(
+                    '[data-disabled]:cursor-not-allowed flex cursor-default items-center justify-between rounded-md px-2 py-2 text-sm transition-all hover:bg-[--accent-a3] active:scale-[98%] disabled:opacity-50',
+                    isActive && 'bg-[--accent-a3]'
+                  )}
+                  onClick={handleNavItemClick}
+                  href={navItem.link}
+                  target={navItem.external ? '_blank' : '_self'}
+                >
+                  <div className="flex items-center justify-start">
+                    <Icon size="18" className="mr-2" stroke="1.5" />
+                    {navItem.name}
+                    {navItem.disabled && <Badge className="ml-2">soon</Badge>}
+                  </div>
+                  <div className="shrink-0">
+                    <IconArrowUpRight size="16" />
+                  </div>
+                </a>
+              ) : (
+                <Link
+                  data-disabled={navItem.disabled}
+                  className={cn(
+                    '[data-disabled]:cursor-not-allowed flex cursor-default items-center justify-between rounded-md px-2 py-2 text-sm transition-all hover:bg-[--accent-a3] active:scale-[98%] disabled:opacity-50',
+                    isActive && 'bg-[--accent-a3]'
+                  )}
+                  onClick={handleNavItemClick}
+                  href={navItem.link}
+                  target={navItem.external ? '_blank' : '_self'}
+                >
+                  <div className="flex items-center justify-start">
+                    <Icon size="18" className="mr-2" stroke="1.5" />
+                    {navItem.name}
+                    {navItem.disabled && <Badge className="ml-2">soon</Badge>}
+                  </div>
+                  <div className="shrink-0">
+                    <IconArrowUpRight size="16" />
+                  </div>
+                </Link>
+              )}
             </li>
           )
         })}
