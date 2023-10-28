ac1bde9f500cf0b3073818dfb68e81b1a624cd03
Craig Hart <i@craigary.net>
2023-10-28 23:38:53 +0800

    add icon in /uses page

--- app/(main)/uses/page.jsx
+++ app/(main)/uses/page.jsx
@@ -3,6 +3,7 @@ import Container from '@/components/container/Container'
 import MyServicesList from '@/components/uses/MyServiceList'
 import { generateMetaData } from '@/lib/metadata'
 import { getMyServices } from '@/lib/notion-next/get-my-services'
+import { IconTools } from '@tabler/icons-react'
 
 const pathName = '/uses'
 export const metadata = generateMetaData(pathName)
@@ -12,7 +13,12 @@ const UsePage = async () => {
 
   return (
     <Container>
-      <Heading title="Use">
+      <Heading title="Use" className="relative">
+        <IconTools
+          className="absolute -right-[10%] top-0 opacity-5"
+          size="80%"
+          stroke={1}
+        />
         <div className="space-y-3 leading-6 tracking-[0.015em] md:w-3/4 md:leading-8">
           <div>We shape our tools, and thereafter our tools shape us.</div>
         </div>
